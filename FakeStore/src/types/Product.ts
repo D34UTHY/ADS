09af79eda34cd9392ec92d59981c27319ab2fc10
Matheus Sant'Ana <98056689+D34UTHY@users.noreply.github.com>
2025-06-12 14:55:45 -0300

    Delete FakeStore directory
    Deletando diretório, para poder colocá-lo na raiz, assim, permitindo o clone do mesmo

--- FakeStore/src/types/Product.ts
+++
@@ -1,8 +0,0 @@
-// Define a estrutura que um produto deve ter (ajuda no TypeScript)
-export interface Product {
-  id: number;
-  title: string;
-  price: number;
-  description: string;
-  image: string;
-}
